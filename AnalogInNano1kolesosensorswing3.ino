d2614f3441dbf979228a18761fb333f80c59c78b
vasiavl <vasiaFPV@gmail.com>
2016-03-12 11:39:32 +0700

    назначение переменной outputRoll

--- AnalogInNano1kolesosensorswing3.ino
+++ AnalogInNano1kolesosensorswing3.ino
@@ -15,7 +15,7 @@ const int analogInPitchPin = A4; // пин для резистора по Pitch
       
 int sensorSwing = 0;//, outputSwingR, outputSwingL  ,returnSensorSwing
 int Roll,tvist,tvistFlag,flagY,flagYF;        
-int outputRoll = 0;
+int outputRoll = 0;//преобразуем значения (приечем обрезанные 823-300) Roll в OutputRoll 1-1023
 int outputRollR = 0;
 int Pitch = 0;        
 int outputPitch = 0;
