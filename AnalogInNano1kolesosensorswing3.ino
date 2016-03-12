444aa46c34de0e03f28cafd921d3b95b729ec47b
vasiavl <vasiaFPV@gmail.com>
2016-03-12 12:22:41 +0700

    Revert "назначение переменной outputRoll"
    This reverts commit d2614f3441dbf979228a18761fb333f80c59c78b.
    
    # Conflicts:
    #	AnalogInNano1kolesosensorswing3.ino

--- AnalogInNano1kolesosensorswing3.ino
+++ AnalogInNano1kolesosensorswing3.ino
@@ -13,12 +13,18 @@ const int analogInPitchPin = A4; // пин для резистора по Pitch
  
 
       
+<<<<<<< HEAD
 int sensorSwing = 0;//значения датчика положение рулевых колес
 
 
 int tvist;// ШИМ рулевого двигателя
 int tvistFlag;//значение реверса рулевого мотора
 int  Roll,outputRoll = 0;//преобразуем значения (приечем обрезанные 823-300) Roll в OutputRoll 1-1023
+=======
+int sensorSwing = 0;//, outputSwingR, outputSwingL  ,returnSensorSwing
+int Roll,tvist,tvistFlag,flagY,flagYF;        
+int outputRoll = 0;
+>>>>>>> parent of d2614f3... назначение переменной outputRoll
 int outputRollR = 0;
 int Pitch = 0;        
 int outputPitch = 0;
